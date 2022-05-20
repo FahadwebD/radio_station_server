ce5cafbf75aa55ead2e32739961007cda693097d
FahadwebD <fahadchowdhury66779@gmail.com>
2022-05-20 22:20:31 +0600

    add channel api post

--- index.js
+++ index.js
@@ -8,3 +8,63 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ffrgt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run (){
+
+    try{
+        await client.connect();
+        const database = client.db('station');
+        const channelCollection = database.collection('channel');
+
+    
+        app.post('/channel/add' , async(req , res)=>{
+            const channel = req.body;
+          const result = await channelCollection.insertOne(channel);
+           res.json(result);
+           
+        })
+    
+    
+         
+       
+      
+
+  
+
+        
+        
+
+    
+        
+ 
+
+        
+
+
+
+ 
+
+   
+
+    }
+    finally{
+        // await client.close();
+    }
+
+
+}
+
+run().catch(console.dir);
+
+
+
+app.get('/', ( req , res)=>{
+    res.send('assignment 12')
+})
+
+app.listen(port , ()=>{
+    console.log(`listening at ${port}`)
+})
